1bb5a0e91737686825294d91837b6fce6c674144
s <ledinhanhtan@gmail.com>
2019-12-14 12:05:40 +0700

    Added Icon

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,19 +4,21 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage stage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample/view/calculator.fxml"));
         Parent root = loader.load();
-        primaryStage.setTitle("Calculator");
-        primaryStage.setScene(new Scene(root, 230, 440));
-        primaryStage.setResizable(false);
-        primaryStage.show();
-        primaryStage.setOnCloseRequest(event -> System.exit(0));
+        stage.setTitle("Calculator");
+        stage.setScene(new Scene(root, 230, 440));
+        stage.setResizable(false);
+        stage.show();
+        stage.getIcons().add(new Image("sample/resource/icon/icon.png"));
+        stage.setOnCloseRequest(event -> System.exit(0));
     }
 
     public static void main(String[] args) {
